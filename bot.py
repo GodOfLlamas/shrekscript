c2d64e8ea6cd556f5f4e8b90c5c544b4fd9cd326
GodOfLlamas <66080971+GodOfLlamas@users.noreply.github.com>
2021-02-13 18:24:04 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ def shrek_1_message(message):
     with open('shrek_1.txt', encoding='utf-8') as f:
         text = f.read()
         for line in text.splitlines():
-            time.sleep(.17)
+            time.sleep(.18)
             if(line == ''):
                 bot.send_message(message.chat.id, '_______________________________________________')
             else:
@@ -32,7 +32,7 @@ def shrek_2_message(message):
     with open('shrek_2.txt', encoding='utf-8') as f:
         text = f.read()
         for line in text.splitlines():
-            time.sleep(.17)
+            time.sleep(.18)
             if(line == ''):
                 bot.send_message(message.chat.id, '_______________________________________________')
             else:
@@ -45,7 +45,7 @@ def shrek_3_message(message):
     with open('shrek_3.txt', encoding='utf-8') as f:
         text = f.read()
         for line in text.splitlines():
-            time.sleep(.17)
+            time.sleep(.18)
             if(line == ''):
                 bot.send_message(message.chat.id, '_______________________________________________')
             else:
@@ -58,7 +58,7 @@ def shrek_4_message(message):
     with open('shrek_4.txt', encoding='utf-8') as f:
         text = f.read()
         for line in text.splitlines():
-            time.sleep(.17)
+            time.sleep(.18)
             if(line == ''):
                 bot.send_message(message.chat.id, '_______________________________________________')
             else:
